5e93129b208f10665ba805bb6a9f6a2dab7030ba
ashikka <ashikagupta28@gmail.com>
2021-05-22 01:48:49 +0530

    fix: console

--- bin/utils.js
+++ bin/utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 const fs = require('fs-extra');
 const path = require('path');
 const chalk = require('chalk');
@@ -5,7 +6,7 @@ const boxen = require('boxen');
 
 const makeCLI = async (cliName, cliPath) => new Promise((resolve, reject) => {
   const myPath = path.join(cliPath, `/${cliName}`);
-  const samplePath = path.join(__dirname, '..', 'sample_cli');
+  const samplePath = path.join(__dirname, '..', 'your-cli');
 
   fs.copy(samplePath, myPath, (err) => {
     if (err) {
@@ -24,8 +25,10 @@ const makeCLI = async (cliName, cliPath) => new Promise((resolve, reject) => {
 });
 
 const showHelp = () => {
-  console.log(boxen('\nUsage: gen-cli <cli-name> <path>\r', { padding: 2, borderColor: 'yellow' }));
-  console.log('\nOptions:\r \n\t--version\t      ' + 'Show version number.' + '\t\t' + '[boolean]\r \n\t--help\t\t      ' + 'Show help.' + '\t\t\t' + '[boolean]\n');
+  const gencli = "\n                       _ _ \n                      | (_)\n  __ _  ___ _ __   ___| |_ \n / _` |/ _ \\ '_ \\ / __| | |\n| (_| |  __/ | | | (__| | |\n \\__, |\\___|_| |_|\\___|_|_|\n  __/ |                    \n |___/                     \n";
+  console.log(chalk.whiteBright(gencli));
+  console.log(boxen('Usage: gen-cli <cli-name> <path>', { padding: 2, borderColor: 'yellow' }));
+  console.log('Options:\r\n\t--version\t      ' + 'Show version number.' + '\t\t' + '[boolean]\r \n\t--help\t\t      ' + 'Show help.' + '\t\t\t' + '[boolean]\n');
 };
 
 module.exports = { makeCLI, showHelp };
