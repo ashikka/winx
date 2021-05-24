bff66a17f9317933e12d0377f427e6a02dba0f74
ashikka <ashikagupta28@gmail.com>
2021-05-24 16:14:03 +0530

    fix: typo

--- your-cli/bin/index.js
+++ your-cli/bin/index.js
@@ -6,7 +6,7 @@ const utils = require('./utils');
 After successful creation of your CLI, follow these steps:
 
 1. Navigate to the directory where you CLI is
-1. In the package.json file, chnage the **name** of your CLI.
+1. In the package.json file, change the **name** of your CLI.
 2. In the same file, inside the key **bin**, change the key of
 subjson from your-cli to the name of your CLI.
 3. Run npm i -g command
