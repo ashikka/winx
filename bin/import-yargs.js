1d9800eef57b914f37309dfb65106f7d2c7416cc
alias-rahil <rahil.kabani.4@gmail.com>
2021-06-05 14:43:23 +0530

    style: beautify error output

--- bin/import-yargs.js
+++ bin/import-yargs.js
@@ -1,10 +1,12 @@
 /* eslint-disable global-require */
+const chalk = require('chalk');
+
 const importYargs = () => {
   try {
     const yargs = require('yargs');
     return yargs;
   } catch (e) {
-    console.log(e.message);
+    console.log(chalk.red(e));
     return process.exit(1);
   }
 };
